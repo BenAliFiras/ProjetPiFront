5078e01ea3009e30d422a2bc04db8af36538287b
Firas <firasbenali65@gmail.com>
2023-07-08 12:19:38 +0100

    erreur

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -45,6 +45,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { OtherUsersComponent } from './other-users/other-users.component';
 import { ModalReclamationComponent } from './modal-reclamation/modal-reclamation.component';
 import { ReservationArchivePageComponent } from './reservation-archive-page/reservation-archive-page.component';
+import { SuccessModalComponent } from './success-modal/success-modal.component';
+import { ErrorModalComponent } from './error-modal/error-modal.component';
 
 
 
@@ -76,7 +78,9 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     NotFoundComponent,
     OtherUsersComponent,
     ModalReclamationComponent,
-    ReservationArchivePageComponent
+    ReservationArchivePageComponent,
+    SuccessModalComponent,
+    ErrorModalComponent
   ],
   imports: [
     CommonModule,
